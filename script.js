2fa05454efebca3be21e842cc00cd51d6c595b96
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-01-26 18:16:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var STLfood = L.map('STLfood').setView([38.6270,-90.1994], 10);
+var Denverfood = L.map('Denverfood').setView([39.808, -105.555], 10);
 
   // load a tile layer
  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
@@ -11,14 +11,12 @@ var STLfood = L.map('STLfood').setView([38.6270,-90.1994], 10);
 }).addTo(STLfood);
 
 
-var marker1 = L.marker([38.649, -90.261]).addTo(STLfood);
-var marker2 = L.marker([38.640, -90.260]).addTo(STLfood);
-var marker3 = L.marker([38.599, -90.237]).addTo(STLfood);
-var marker4 = L.marker([38.646, -90.262]).addTo(STLfood);
-var marker5 = L.marker([38.560, -90.252]).addTo(STLfood);
+var marker1 = L.marker([39.753, -105.053]).addTo(Denverfood);
+var marker2 = L.marker([39.740, -104.983]).addTo(Denverfood);
+var marker3 = L.marker([39.959, -105.331]).addTo(Denverfood);
 
-marker1.bindPopup("<b>Resturant 1</b><br>Jeni's Splendid Ice Creams").openPopup();
-marker2.bindPopup("<b>Resturant 2</b><br>PokeDoke").openPopup();
-marker3.bindPopup("<b>Resturant 1</b><br>Sheesh").openPopup();
-marker4.bindPopup("<b>Resturant 2</b><br>Taste Of Lebanon").openPopup();
-marker5.bindPopup("<b>Resturant 1</b><br>Pho Grand").openPopup();
+
+marker1.bindPopup("<b>Resturant 1</b><br>US Thai Cafe").openPopup();
+marker2.bindPopup("<b>Resturant 2</b><br>Phở-natic").openPopup();
+marker3.bindPopup("<b>Resturant 1</b><br>TACOS TEQUILA WHISKEY").openPopup();
+
